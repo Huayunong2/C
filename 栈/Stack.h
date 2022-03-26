1ea75790eb30a27110c7bf729cc614e17c9dfb91
编程不会，不会就是不会 <166589348@qq.com>
2022-03-26 14:01:30 +0000

    3.26

---
+++ 栈/Stack.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+typedef int STDataType;
+typedef struct Stack
+{
+	STDataType* a;
+	int top; //’ª∂•Œª÷√
+	int capacity; //»›¡ø
+}ST;
+void StackInit(ST* ps);
+void StackDestory(ST* ps);
+void StackPush(ST* ps, STDataType x);
+void StackPop(ST* ps);
+bool StackEmpty(ST* ps);
+STDataType StackTop(ST* ps);
---
+++ 栈/test.c
@@ -0,0 +1,20 @@
+#include "Stack.h"
+void testStack()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	while (!StackEmpty(&st))
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+}
+int main()
+{
+	testStack();
+	return 0;
+}
