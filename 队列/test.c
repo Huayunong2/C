0c3767a364978dc723dcecfa024aa5d3ec08a62e
编程不会，不会就是不会 <166589348@qq.com>
2022-03-26 14:27:03 +0000

    3.26

---
+++ 队列/test.c
@@ -0,0 +1,22 @@
+#include "Queue.h"
+void Test()
+{
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+	QueuePush(&q, 5);
+	while (!QueueEmpty(&q))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	printf("\n");
+}
+int main()
+{
+	Test();
+	return 0;
+}
