2143f2d0220e064cd8086499dcebfdad2ea571fc
编程不会，不会就是不会 <166589348@qq.com>
2022-04-07 19:46:38 +0000

    4.8

---
+++ 关于传参问题.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void Test1(int* pp1, int* pp2)
+{
+	int tmp = pp1;
+	pp1 = pp2;
+	pp2 = tmp;
+}
+void Test2(int** pp1, int** pp2)
+{
+	*pp1 = 0x
+}
+int main()
+{
+	int a = 20;
+	int b = 30;
+	int* p1 = &a;
+	int* p2 = &b;
+
+
+	printf("p1:%p\n", p1);
+	printf("p2:%p\n", p2);
+	Test1(p1, p2);
+	printf("----------传入后----------\n");
+	printf("p1:%p\n", p1);
+	printf("p2:%p\n", p2);
+
+	printf("\n\n\n");
+
+	printf("p1:%p\n", p1);
+	printf("p2:%p\n", p2);
+	Test2(&p1,&p2);
+	printf("----------传入后----------\n");
+	printf("p1:%p\n", p1);
+	printf("p2:%p\n", p2);
+	//结论:要改变谁就传谁的地址，接收谁的地址就加一层指针
+	return 0;
+}
