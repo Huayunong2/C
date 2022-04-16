1e3370db49d7967f4838f34d83fa420cf4fa349a
编程不会，不会就是不会 <166589348@qq.com>
2022-04-16 00:41:41 +0000

    4.16

---
+++ 二叉树/二叉树链式结构/Queue.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+struct BinaaryTreeNode;
+
+typedef struct BinaryTreeNode* QDataType;
+
+typedef struct QueueNode
+{
+	QDataType data;
+	struct QueueNode* next;
+}QNode; //定义节点
+typedef struct Queue
+{
+	QNode* head;
+	QNode* tail;
+	//size_t size = 0; //插入数据++，删除数据--，初始化0
+}Queue; //定义头尾
+void QueueInit(Queue* pq);
+void QueueDestory(Queue* pq);
+void QueuePush(Queue* pq, QDataType x);
+void QueuePop(Queue* pq);
+bool QueueEmpty(Queue* pq);
+size_t QueueSize(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
